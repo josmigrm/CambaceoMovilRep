2264cd59abd012bb88cf23196e6615e6ed65d085
edgar <derag.rubio@gmail.com>
2015-07-01 23:22:15 -0500

    Se agrega llamado a backend para autenticacion en el login

--- app/src/main/java/com/cablevision/cambaceomovil/LoginActivity.java
+++ app/src/main/java/com/cablevision/cambaceomovil/LoginActivity.java
@@ -2,16 +2,26 @@ package com.cablevision.cambaceomovil;
 
 
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
+import android.widget.Toast;
+import android.app.AlertDialog;
 
 public class LoginActivity extends Activity implements View.OnClickListener {
-
+    String IP="192.168.100.6";
+    String PUERTO="9090";
+    final String url ="http://"+IP+":"+PUERTO+"/RESTfulExample/rest/CambaceoMovil/login?user=";
+    String resultRequest = "false";
     Button bLogin;
     EditText etUsername, etPassword;
 
@@ -33,17 +43,53 @@ public class LoginActivity extends Activity implements View.OnClickListener {
         }
     }
 
+    public String getResultRequest() {
+        return resultRequest;
+    }
+
+    public void setResultRequest(String resultRequest) {
+        this.resultRequest = resultRequest;
+    }
+
     private void authenticateUser() {
-        if("admin".equals(etUsername.getText().toString()) && "1234".equals(etPassword.getText().toString())){
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String user = etUsername.getText().toString();
+        String pass = etPassword.getText().toString();
+        String urlFinal= url+user+"&password="+pass;
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, urlFinal,
+            new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    setResultRequest(response);
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(LoginActivity.this, "Error al cargar los datos: "+error.toString(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        );
+        queue.add(stringRequest);
+
+        if(resultRequest.equals("true")){
             Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
+        } else if(resultRequest.equals("false")){
+            AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+            dialogBuilder.setMessage("Error del sistema intente en un momento");
+            dialogBuilder.setPositiveButton("Ok", null);
+            dialogBuilder.show();
         } else {
             AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
-            dialogBuilder.setMessage("Credenciales incorrectas");
+            dialogBuilder.setMessage(getResultRequest());
             dialogBuilder.setPositiveButton("Ok", null);
             dialogBuilder.show();
         }
+
     }
+
+
 
     private boolean validaCampos(){
         boolean result = true;
